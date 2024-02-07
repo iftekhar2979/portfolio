d0dcfce8a367750d667098943d8636b221888b33
ifti2979 <salminifti79@gmail.com>
2024-02-07 21:41:29 +0600

    html css and picture added

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    theme: {
+      colors: {
+        bg_primary:"#ef7efe1",
+        bg_secondary:"#1fc667",
+        black:"#191919",
+        green:"#1fc667",
+        white:"#1fc667"
+
+      }
+    }
+  }
